94bab39058f09cc7a381dba97110862b5ebc4447
Perkles <nathantadeu.sc@gmail.com>
2023-01-18 21:41:41 -0300

    Entendendo sobre Hierarquia de Codigo

---
+++ Introducao/Java/java-anatomia-classes/src/BoletimEstudantil.java
@@ -0,0 +1,12 @@
+public class BoletimEstudantil {
+    //Identacao sobre codigo, exercendo a hierarquia de codigo//
+    public static void main(String[] args) {
+        int mediaFinal = 9;
+            if (mediaFinal < 6)
+            System.out.println("REPROVADO");
+        if (mediaFinal==6)
+            System.out.println("PROVA MINERVA");
+        else
+            System.out.println("APROVADO");
+    }
+}
