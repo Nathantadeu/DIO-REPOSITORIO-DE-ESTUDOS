c075c2b3e4da2ec78b51330ea69f28aa1cf2f80e
Perkles <nathantadeu.sc@gmail.com>
2023-01-24 23:36:14 -0300

    Exemplo de variaveis

---
+++ Introducao/Java/java-anatomia-classes/src/TiposVariaveis.java
@@ -0,0 +1,10 @@
+public class TiposVariaveis {
+ public static void main(String[] args) {
+    byte idade = 123;
+    short ano = 2021;
+    int cep = 21070333;
+    long cpf = 98765432109L; //Se comecar com zero, talvez tenha que ser outro tipo
+    float pi = 3.14F;//Se comecar com zero, talvez tenha que ser outro tipo
+    double salario = 1275.33;
+ }   
+}
