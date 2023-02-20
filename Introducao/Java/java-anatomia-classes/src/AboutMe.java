44b4a5eded6782bdf524a23132e611103b378718
Perkles <nathantadeu.sc@gmail.com>
2023-02-20 15:45:40 -0300

    Melhorando-File-AboutMe

--- Introducao/Java/java-anatomia-classes/src/AboutMe.java
+++ Introducao/Java/java-anatomia-classes/src/AboutMe.java
@@ -1,15 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AboutMe {
     public static void main(String[] args) {
-        
-        String nome = args[0];
-        String sobrenome = args [1];
-        int idade = Integer.valueOf(args[2]);
-        double altura = Double.valueOf(args[3]);
-        double peso = Double.valueOf(args[4]);
+        //Criando obejeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println(" Ola, me chamo " + nome + " " + sobrenome);
+        
+
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade");
+        int idade = scanner.nextInt();
+        
+        System.out.println("Digite sua altura");
+        double altura = scanner.nextDouble();
+
+        System.out.println(" Ola, Meu chamo " + nome + " " + sobrenome);
         System.out.println(" Tenho " + idade + " anos");
-        System.out.println(" Minha altura e " + altura + " cm");
-        System.out.println(" Meu peso e " + peso + " kg");
+        System.out.println(" Minha altura e " + altura + "cm");
     }
 }
